344466acaa53ff35c6df52193a328b66f6610396
sh0327akg <shota.gk0327m.tf@gmail.com>
2023-05-06 23:50:56 +0900

    [fix]記述位置の修正

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,15 +3,11 @@ import './message';
 import './rating';
 import { like, unlike } from "./like";
 import { switchTab } from './tabswitch';
-
+import Alpine from 'alpinejs';
 
 window.like = like;
 window.unlike = unlike;
-
 window.switchTab = switchTab;
 
-import Alpine from 'alpinejs';
-
 window.Alpine = Alpine;
-
 Alpine.start();
