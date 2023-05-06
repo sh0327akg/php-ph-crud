dae8389e8bed32b03cea5c8c3d83cddbcf8ea9f3
sh0327akg <shota.gk0327m.tf@gmail.com>
2023-05-06 12:29:23 +0900

    [add]ビューをタブで切り変えるためのJSを追加する

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,9 +2,13 @@ import './bootstrap';
 import './message';
 import './rating';
 import { like, unlike } from "./like";
+import { switchTab } from './tabswitch';
+
 
 window.like = like;
 window.unlike = unlike;
+
+window.switchTab = switchTab;
 
 import Alpine from 'alpinejs';
 
