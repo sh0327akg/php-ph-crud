682884d8ea809cb937f90ce14894621c1ce302fa
sh0327akg <shota.gk0327m.tf@gmail.com>
2023-05-07 13:10:06 +0900

    [add]policyのエラーを修正する

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 use App\Models\Post;
 use App\Policy\PostPolicy;
+use Illuminate\Support\Facades\Gate;
 
 // use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
